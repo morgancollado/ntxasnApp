0d59e9a19ede77a133ba1f69d90166dcc4920b5e
Morgan Collado <morgan.collado@gmail.com>
2021-02-21 18:10:57 -0600

    creates rides, database seeds not yet working though.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_031618) do
+ActiveRecord::Schema.define(version: 2021_02_21_230502) do
+
+  create_table "rides", force: :cascade do |t|
+    t.datetime "date_time"
+    t.string "location_to"
+    t.string "location_from"
+    t.string "appointment_type"
+    t.integer "passenger_id"
+    t.integer "driver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ride.destroy_all
+User.destroy_all
+
+require 'faker'
+require 'byebug'
+
+user = User.first || User.create(email: 'test@test.com', password: 'password', password_confirmation: 'password')
+
+user2 = User.create(email: 'morgan.collado@gmail.com', password: 'hello', password_confirmation: 'hello')
+
+ride = Ride.create(
+    date_time: Faker::Time.forward(days: 15, period: :morning),
+    location_to: Faker::Address.full_address,
+    location_from: Faker::Address.full_address,
+    appointment_type: "Consult",
+    passenger_id: 1,
+    driver_id: 2
+)
